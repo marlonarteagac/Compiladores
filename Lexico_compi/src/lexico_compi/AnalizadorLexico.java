902ebb48d047b7c0c4e0297a4b3af1ad6178e8be
MarlonArteaga <MarlonART>
2024-02-25 20:23:30 -0500

    funcionando parte del lexico y sintactico

--- Lexico_compi/src/lexico_compi/AnalizadorLexico.java
+++ Lexico_compi/src/lexico_compi/AnalizadorLexico.java
@@ -3,13 +3,15 @@ package lexico_compi;
 import java.util.Scanner;
 
 public class AnalizadorLexico {
-//agregue el comentario para actualizar en git
+    // agregue el comentario para actualizar en git
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//pepitoperez
+        // pepitoperez
         System.out.println("Ingrese un texto (máximo 20 caracteres): ");
         String texto = scanner.nextLine();
         String resultado = "";
+        int var_cont = 0;
+        int bandera = 0;
 
         if (texto.isEmpty()) {
             resultado = "Por favor, ingrese un texto.";
@@ -17,67 +19,110 @@ public class AnalizadorLexico {
             char[] arreglo_texto = texto.toCharArray();
             int[] identificadores = new int[arreglo_texto.length]; // Tamaño del array de identificadores
 
-            // Contador para llevar el control de la cantidad de identificadores encontrados
-            int contador = 0;
-            try {
-                  for (int i = 0; i < arreglo_texto.length; i++) {
+            char[] operadoresEncontrados = new char[arreglo_texto.length];
+            int contadorOperadores = 0;
+
+            for (int i = 0; i < arreglo_texto.length; i++) {
                 switch (arreglo_texto[i]) {
-                    case '$':
-                    case '#':
-                    case '!':
                     case '+':
                     case '-':
                     case '*':
                     case '/':
-                        identificadores[contador] = i;
+                        operadoresEncontrados[contadorOperadores] = arreglo_texto[i];
+                        contadorOperadores++;
+                        // System.out.println("encontradors" + arreglo_texto[i]);
+
+                }
+
+            }
+
+            // Contador para llevar el control de la cantidad de identificadores encontrados
+            int contador = 0;
+            try {
+                for (int i = 0; i < arreglo_texto.length; i++) {
+                    switch (arreglo_texto[i]) {
+                        case '$':
+                        case '#':
+                        case '!':
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            identificadores[contador] = i;
+                            contador++;
+                            break;
+                    }
+
+                    if (arreglo_texto.length - 1 == i) {
+                        identificadores[contador] = i + 1;
                         contador++;
-                        break;
+                    }
                 }
 
-                if (arreglo_texto.length - 1 == i) {
-                    identificadores[contador] = i + 1;
-                    contador++;
-                }
-            }
-                 
             } catch (Exception e) {
+                bandera = 1;
+                Mensaje(bandera);
                 System.out.println("La variable no puede estar vacia try catch");
-                
-            }
-            // Obtener posiciones de los identificadores
-          
 
-            //con este while verifico los caracteres para decir si es valido o no
+            }
+
+            // con este while verifico los caracteres para decir si es valido o no
+
             int k = 0;
             while (k < arreglo_texto.length) {
                 char caracter_actual = arreglo_texto[k];
                 char caracter_siguiente = (k + 1 < arreglo_texto.length) ? arreglo_texto[k + 1] : ' ';
+                // char caracter3 = (k + 2 < arreglo_texto.length) ? arreglo_texto[k + 2] : ' ';
 
+                if (esidentificador(caracter_actual) && esLetra(caracter_siguiente)) {
+                    System.out.println("paso 1");
+                    k++;
 
-//               System.out.println("k siguiente es :" + caracter_siguiente);
-
-                if (esOperador(caracter_actual) && esLetra(caracter_siguiente)) {
-                    System.out.println("no se puede letra despues de operador, flata identificador de variable");
-                    break;
-          
-                }else if (esidentificador(caracter_siguiente) && esLetra(caracter_siguiente)) {
-                    System.out.println("letra despues de identificador");
-                    continue;
-                }else if (!esidentificador(caracter_actual) && !esLetra(caracter_actual) && !esOperador(caracter_actual) && caracter_actual!='_') {
-                    System.out.println("error: hay un caracter no valido");
-                    break;
+                }else if (esidentificador(caracter_actual)) {
+                    if (esNumero(caracter_siguiente)) {
+                        bandera = 1;
+                        break;
+                    }
                 }
- 
+                
+                else if (esOperador(caracter_actual)) {
+                    if (esLetra(caracter_siguiente)) {
+                        bandera = 1;
+                        break;
+                    }
+                }else if (esidentificador(caracter_actual)) {
+                    if (esidentificador(caracter_siguiente)) {
+                        bandera = 1;
+                        break;
+                    }
+                }
+                
+
                 k++;
-          
+
+                System.out.println(caracter_siguiente);
             }
 
+            System.out.println("la bander vale: " + bandera);
+            if (bandera <= 0) {
+                bandera = 0;
+                Mensaje(bandera);
+            } else {
+                bandera = 1;
+                Mensaje(bandera);
+            }
             // Validar las variables
-            for (int i = contador - 1; i > 0; i--) {
+
+            // ARREGLO PARA GUARDAR LAS VARIABLES COMPLETAS}
+
+            String[] arregloVariables = new String[contador];
+
+            for (int i = contador - 1; i > 0; i--) {// INICIO DEL FOR PRINCIPAL
                 char identificadorAnterior = arreglo_texto[identificadores[i - 1]];
                 if (identificadorAnterior == '+' || identificadorAnterior == '-'
                         || identificadorAnterior == '/' || identificadorAnterior == '*') {
                     continue;
+
                 }
 
                 int finVar = identificadores[i];
@@ -93,6 +138,26 @@ public class AnalizadorLexico {
 
                 }
 
+                // // PARA GUARDAR LA VARIABLE COMPLETA EN EL ARREGLO PARA ORDENAR
+                int a_finVar = identificadores[i];
+                int a_inicioVar = identificadores[i - 1];
+                int a_longitudVariable = a_finVar - a_inicioVar;
+
+                char[] a_variable = new char[a_longitudVariable];
+
+                // LA VARIABLE QUE TENDRA EL IDENTIFICADOR
+                String a_mv = "";
+
+                for (int j = a_inicioVar; j < a_finVar; j++) {
+                    a_variable[j - a_inicioVar] = arreglo_texto[j];
+                    a_mv += arreglo_texto[j];
+
+                    arregloVariables[i - 1] = a_mv;
+
+                }
+
+                // AQUI ME ESTA IMPRIMIENDO BIEN LAS VARIABLES CADA QUE DA LA VUELTA EL CICLO
+
                 if (identificadorAnterior == '$') {
                     resultado += "Variable tipo string: ";
                 } else if (identificadorAnterior == '#') {
@@ -104,20 +169,37 @@ public class AnalizadorLexico {
                 if (!validarvariable(variable)) {
                     resultado += " - no válida\n";
                 } else {
-                    resultado += " - válida " + mv + "\n";
+                    resultado += "variable: " + mv + "\n";
                 }
-               
-   
+
+                var_cont += 1;
+
+                // HASTA AQUI LLEGA LA CUESTION DE LAS VARIABLES
+
             }
+
+            // FIN DEL FOR PRINCIPAL
+
+            // IMPRIMIR LAS VARIABLES COMPLETAS
+            for (int p = 0; p < arregloVariables.length; p++) {
+                if (arregloVariables[p] != null) {
+                    System.out.println("Variable " + (p + 1) + ": " + arregloVariables[p]);
+                }
+
+            }
+            System.out.println("contador operadores: " + contadorOperadores);
+            System.out.println("contador variables: " + var_cont);
+
         }
-        System.out.println("" + resultado);
+
     }
+
+    // System.out.println("" + resultado + "");
 
     public static boolean validarvariable(char[] caracteres) {
 
         // Regla adicional: La variable no puede estar vacía
         if (caracteres.length == 0) {
-            System.out.println("La variable no puede estar vacía");
             return false;
         }
 
@@ -125,20 +207,23 @@ public class AnalizadorLexico {
             char c = caracteres[i];
 
             // Regla 1: No puede empezar por un valor numérico
-            if (i == 0 && (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9')) {
+            if (i == 0 && (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7'
+                    || c == '8' || c == '9')) {
                 System.out.println("No puede empezar por un valor numérico la variable");
                 return false;
             }
 
             // Regla 2: Debe empezar por una letra
-            if (i == 0 && !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c=='_')) {
+            if (i == 0 && !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_')) {
                 System.out.println("Debe empezar por una letra la variable");
                 return false;
             }
 
-            // Regla 3: Los caracteres pueden ser letras, dígitos, guiones bajos o operadores
+            // Regla 3: Los caracteres pueden ser letras, dígitos, guiones bajos o
+            // operadores
             if (!(Character.isLetterOrDigit(c) || c == '_' || c == '+' || c == '-' || c == '*')) {
-                System.out.println("Los caracteres pueden ser letras, dígitos, guiones bajos o operadores y no puede tener espacio");
+                System.out.println(
+                        "Los caracteres pueden ser letras, dígitos, guiones bajos o operadores y no puede tener espacio");
                 return false;
             }
 
@@ -153,10 +238,24 @@ public class AnalizadorLexico {
     private static boolean esOperador(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
+    private static boolean esNumero(char c) {
+        return (c >= '0' && c <= '9') ;
+    }
+
+
 
     private static boolean esidentificador(char c) {
         return c == '$' || c == '#' || c == '!';
     }
-    
-  
+
+    public static void Mensaje(int bandera) {
+        if (bandera == 0) {
+            System.out.println("EL TEXTO ES VALIDO");
+
+        } else {
+            System.out.println("EL TEXTO NO ES VALIDO");
+
+        }
+    }
+
 }
