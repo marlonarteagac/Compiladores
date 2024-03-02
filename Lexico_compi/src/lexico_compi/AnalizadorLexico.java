7b1ab388930325cc512a58ebac4a74cd49b5a36f
Marlon Art <marlon.arteagac.u@gmail.com>
2024-03-02 02:23:16 -0500

    se agrega la vista y se ajusta el lexico y sintactico, funcionando ambos

--- Lexico_compi/src/lexico_compi/AnalizadorLexico.java
+++ Lexico_compi/src/lexico_compi/AnalizadorLexico.java
@@ -1,5 +1,6 @@
-package lexico_compi;
+ package lexico_compi;
 
+import java.text.BreakIterator;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,217 +15,245 @@ public class AnalizadorLexico {
         int var_cont = 0;
         int bandera = 0;
 
-        if (texto.isEmpty()) {
-            resultado = "Por favor, ingrese un texto.";
-        } else {
-            char[] arreglo_texto = texto.toCharArray();
-            int[] identificadores = new int[arreglo_texto.length]; // Tamaño del array de identificadores
-            int l = arreglo_texto.length;
-            String[] operadoresEncontrados = new String[arreglo_texto.length];
-            int contadorOperadores = 0;
+        if (texto.length() < 20) {
+            System.out.println("" + validarLongitudTexto(texto));
+            if (texto.isEmpty()) {
+                resultado = "Por favor, ingrese un texto.";
 
-            for (int i = 0; i < arreglo_texto.length; i++) {
-                switch (arreglo_texto[i]) {
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                        operadoresEncontrados[i] = String.valueOf(arreglo_texto[i]);
-                        contadorOperadores++;
-                        // System.out.println("operadores encontrados" + arreglo_texto[i] );
+            } else {
+                char[] arreglo_texto = texto.toCharArray();
+                int[] identificadores = new int[arreglo_texto.length]; // Tamaño del array de identificadores
+                // int l = arreglo_texto.length;
+                String[] operadoresEncontrados = new String[arreglo_texto.length];
+                int contadorOperadores = 0;
 
-                }
-
-            }
-
-            // Contador para llevar el control de la cantidad de identificadores encontrados
-            int contador = 0;
-            try {
                 for (int i = 0; i < arreglo_texto.length; i++) {
                     switch (arreglo_texto[i]) {
-                        case '$':
-                        case '#':
-                        case '!':
                         case '+':
                         case '-':
                         case '*':
                         case '/':
-                            identificadores[contador] = i;
-                            contador++;
-                            break;
+                            operadoresEncontrados[i] = String.valueOf(arreglo_texto[i]);
+                            contadorOperadores++;
+                            // System.out.println("operadores encontrados" + arreglo_texto[i] );
+
                     }
 
-                    if (arreglo_texto.length - 1 == i) {
-                        identificadores[contador] = i + 1;
-                        contador++;
-                    }
                 }
 
-            } catch (Exception e) {
-                bandera = 1;
-                Mensaje(bandera);
-                System.out.println("La variable no puede estar vacia try catch");
+                // Contador para llevar el control de la cantidad de identificadores encontrados
+                int contador = 0;
+                try {
+                    for (int i = 0; i < arreglo_texto.length; i++) {
+                        switch (arreglo_texto[i]) {
+                            case '$':
+                            case '#':
+                            case '!':
+                            case '+':
+                            case '-':
+                            case '*':
+                            case '/':
+                                identificadores[contador] = i;
+                                contador++;
+                                break;
+                        }
 
-            }
+                        if (arreglo_texto.length - 1 == i) {
+                            identificadores[contador] = i + 1;
+                            contador++;
+                        }
+                    }
 
-            // con este while verifico los caracteres para decir si es valido o no
+                } catch (Exception e) {
+                    bandera = 1;
+                    Mensaje(bandera);
+                    System.out.println("La variable no puede estar vacia try catch");
 
-            int k = 0;
-            while (k < arreglo_texto.length) {
-                char caracter_actual = arreglo_texto[k];
-                char caracter_siguiente = (k + 1 < arreglo_texto.length) ? arreglo_texto[k + 1] : ' ';
-                // char caracter3 = (k + 2 < arreglo_texto.length) ? arreglo_texto[k + 2] : ' ';
+                }
 
-                if (esidentificador(caracter_actual) && esLetra(caracter_siguiente)) {
-                    // System.out.println("paso 1");
+                // con este while verifico los caracteres para decir si es valido o no
+
+                int k = 0;
+                while (k < arreglo_texto.length) {
+                    char caracter_actual = arreglo_texto[k];
+                    char caracter_siguiente = (k + 1 < arreglo_texto.length) ? arreglo_texto[k + 1] : ' ';
+                    // char caracter3 = (k + 2 < arreglo_texto.length) ? arreglo_texto[k + 2] : ' ';
+
+                    if (esidentificador(caracter_actual) && esLetra(caracter_siguiente)) {
+                        // System.out.println("paso 1");
+                        k++;
+
+                    }
+                    if (!esidentificador(caracter_actual)
+                            && !esLetra(caracter_actual)
+                            && !esNumero(caracter_actual)
+                            && !esOperador(caracter_actual)) {
+
+                        bandera = 1;
+                        break;
+
+                    }
+
+                    if (esidentificador(caracter_actual)) {
+                        if (esidentificador(caracter_siguiente)) {
+                            bandera = 1;
+                            break;
+                        }
+                    }
+                    if (esidentificador(caracter_actual)) {
+                        if (esNumero(caracter_siguiente)) {
+                            bandera = 1;
+                            break;
+                        }
+                    }
+
+                    if (esOperador(caracter_actual)) {
+                        if (esLetra(caracter_siguiente)) {
+                            bandera = 1;
+                            break;
+                        }
+
+                    }
+
                     k++;
 
-                } else if (esidentificador(caracter_actual)) {
-                    if (esNumero(caracter_siguiente)) {
-                        bandera = 1;
+                }
+
+                System.out.println("la bander vale: " + bandera);
+                if (bandera <= 0) {
+                    bandera = 0;
+                    Mensaje(bandera);
+                } else if (bandera > 0) {
+                    bandera = 1;
+                    Mensaje(bandera);
+
+                }
+
+                // Validar las variables
+
+                // ARREGLO PARA GUARDAR LAS VARIABLES COMPLETAS}
+
+                String[] arregloVariables = new String[contador];
+
+                for (int i = contador - 1; i > 0; i--) {// INICIO DEL FOR PRINCIPAL
+                    char identificadorAnterior = arreglo_texto[identificadores[i - 1]];
+                    if (identificadorAnterior == '+' || identificadorAnterior == '-'
+                            || identificadorAnterior == '/' || identificadorAnterior == '*') {
+                        continue;
+
+                    }
+                    // SACAR LAS VARIABLES DESPUES DEL IDENTIFICADOR
+                    int finVar = identificadores[i];
+                    int inicioVar = identificadores[i - 1] + 1;
+                    int longitudVariable = finVar - inicioVar;
+
+                    char[] variable = new char[longitudVariable];
+                    String mv = "";
+
+                    for (int j = inicioVar; j < finVar; j++) {
+                        variable[j - inicioVar] = arreglo_texto[j];
+                        mv += arreglo_texto[j];
+
+                    }
+                    if (variable.length <= 5) {
+                        System.out.println("tamaño valido " + mv);
+                    } else {
+                        System.out.println("variable invalida " + mv);
                         break;
                     }
-                }
+                    // System.out.println("variable: " + mv);
+                    // // PARA GUARDAR LA VARIABLE COMPLETA EN EL ARREGLO PARA ORDENAR CON EL
+                    // IDENTIFICADOR
+                    int a_finVar = identificadores[i];
+                    int a_inicioVar = identificadores[i - 1];
+                    int a_longitudVariable = a_finVar - a_inicioVar;
 
-                else if (esOperador(caracter_actual)) {
-                    if (esLetra(caracter_siguiente)) {
-                        bandera = 1;
-                        break;
+                    char[] a_variable = new char[a_longitudVariable];
+
+                    // LA VARIABLE QUE TENDRA EL IDENTIFICADOR
+                    String a_mv = "";
+
+                    for (int j = a_inicioVar; j < a_finVar; j++) {
+                        a_variable[j - a_inicioVar] = arreglo_texto[j];
+                        a_mv += arreglo_texto[j];
+
+                        arregloVariables[i - 1] = a_mv;
+
                     }
-                } else if (esidentificador(caracter_actual)) {
-                    if (esidentificador(caracter_siguiente)) {
-                        bandera = 1;
-                        break;
+
+                    // AQUI ME ESTA IMPRIMIENDO BIEN LAS VARIABLES CADA QUE DA LA VUELTA EL CICLO
+
+                    if (identificadorAnterior == '$') {
+                        resultado += "Variable tipo string: ";
+                    } else if (identificadorAnterior == '#') {
+                        resultado += "Variable tipo numérica: ";
+                    } else if (identificadorAnterior == '!') {
+                        resultado += "Variable tipo booleana: ";
                     }
-                }
 
-                k++;
+                    if (!validarvariable(variable)) {
+                        resultado += " - no válida\n";
+                    } else {
+                        resultado += "variable: " + mv + "\n";
+                    }
 
-                System.out.println(caracter_siguiente);
-            }
+                    var_cont += 1;
 
-            System.out.println("la bander vale: " + bandera);
-            if (bandera <= 0) {
-                bandera = 0;
-                Mensaje(bandera);
-            } else {
-                bandera = 1;
-                Mensaje(bandera);
-            }
-            // Validar las variables
+                    // HASTA AQUI LLEGA LA CUESTION DE LAS VARIABLES
 
-            // ARREGLO PARA GUARDAR LAS VARIABLES COMPLETAS}
+                } // FIN DEL FOR PRINCIPAL
 
-            String[] arregloVariables = new String[contador];
+                // IMPRIMIR LAS VARIABLES COMPLETAS
+                // for (int p = 0; p < arregloVariables.length; p++) {
+                // if (arregloVariables[p] != null) {
+                // System.out.println("Variable " + (p + 1) + ": " + arregloVariables[p]);
+                // }
 
-            for (int i = contador - 1; i > 0; i--) {// INICIO DEL FOR PRINCIPAL
-                char identificadorAnterior = arreglo_texto[identificadores[i - 1]];
-                if (identificadorAnterior == '+' || identificadorAnterior == '-'
-                        || identificadorAnterior == '/' || identificadorAnterior == '*') {
-                    continue;
+                // }
+                System.out.println("contador operadores: " + contadorOperadores);
+                System.out.println("contador variables: " + var_cont);
 
-                }
-//SACAR LAS VARIABLES DESPUES DEL IDENTIFICADOR
-                int finVar = identificadores[i];
-                int inicioVar = identificadores[i - 1] + 1;
-                int longitudVariable = finVar - inicioVar;
-
-                char[] variable = new char[longitudVariable];
-                String mv = "";
-
-                for (int j = inicioVar; j < finVar; j++) {
-                    variable[j - inicioVar] = arreglo_texto[j];
-                    mv += arreglo_texto[j];
+                System.out.println("ahora a ordenar");
+                // UNIR LOS ARREGLOS EN UNO SOLO ARREGLO PARA ORDENARLOS
+                String[] nuevoArreglo = unirArreglos(eliminarNull(arregloVariables),
+                        eliminarNull(operadoresEncontrados));
+                System.out.println("arreglo nuevoArreglo" + Arrays.toString(nuevoArreglo));
+                for (int j = 0; j < nuevoArreglo.length; j++) {
 
                 }
+                // ORDENAR LOS CARACTERES DE FORMA VARIABLE OPERADOR VARIABLE
+                String[] ordenado = new String[nuevoArreglo.length];
+                int j = 0;
+                int n = 1;
+                for (int i = 0; i < nuevoArreglo.length; i++) {
 
-                // // PARA GUARDAR LA VARIABLE COMPLETA EN EL ARREGLO PARA ORDENAR CON EL IDENTIFICADOR
-                int a_finVar = identificadores[i];
-                int a_inicioVar = identificadores[i - 1];
-                int a_longitudVariable = a_finVar - a_inicioVar;
+                    String c = (nuevoArreglo[i]);
+                    System.out.println("VALOR A GUARDAR " + c);
+                    if ((String.valueOf(c).equals("+")) || (String.valueOf(c).equals("-"))
+                            || (String.valueOf(c).equals("*")) || (String.valueOf(c).equals("/"))) {
+                        ordenado[n] = nuevoArreglo[i];
+                        n = n + 2;
+                    } else {
 
-                char[] a_variable = new char[a_longitudVariable];
+                        ordenado[j] = nuevoArreglo[i];
+                        j = j + 2;
+                    }
 
-                // LA VARIABLE QUE TENDRA EL IDENTIFICADOR
-                String a_mv = "";
-
-                for (int j = a_inicioVar; j < a_finVar; j++) {
-                    a_variable[j - a_inicioVar] = arreglo_texto[j];
-                    a_mv += arreglo_texto[j];
-
-                    arregloVariables[i - 1] = a_mv;
-
+                    // System.out.println("----------------"+nuevoArreglo[j]+"----------------"+ordenado[i]+"----------------");
                 }
 
-                // AQUI ME ESTA IMPRIMIENDO BIEN LAS VARIABLES CADA QUE DA LA VUELTA EL CICLO
+                System.out.println(Arrays.toString(ordenado));
 
-                if (identificadorAnterior == '$') {
-                    resultado += "Variable tipo string: ";
-                } else if (identificadorAnterior == '#') {
-                    resultado += "Variable tipo numérica: ";
-                } else if (identificadorAnterior == '!') {
-                    resultado += "Variable tipo booleana: ";
-                }
+                System.out.println("arreglo ORDENADO de operadores y tipos de variables");
 
-                if (!validarvariable(variable)) {
-                    resultado += " - no válida\n";
-                } else {
-                    resultado += "variable: " + mv + "\n";
-                }
+                for (int h = 0; h < nuevoArreglo.length; h++) {
+                    System.out.println("posición " + h + " valor " + ordenado[h]);
 
-                var_cont += 1;
-
-                // HASTA AQUI LLEGA LA CUESTION DE LAS VARIABLES
-
-            } // FIN DEL FOR PRINCIPAL
-
-            // IMPRIMIR LAS VARIABLES COMPLETAS
-            for (int p = 0; p < arregloVariables.length; p++) {
-                if (arregloVariables[p] != null) {
-                    System.out.println("Variable " + (p + 1) + ": " + arregloVariables[p]);
                 }
 
             }
-            System.out.println("contador operadores: " + contadorOperadores);
-            System.out.println("contador variables: " + var_cont);
-
-            System.out.println("ahora a ordenar");
-            // UNIR LOS ARREGLOS EN UNO SOLO ARREGLO PARA ORDENARLOS
-            String[] nuevoArreglo = unirArreglos(eliminarNull(arregloVariables), eliminarNull(operadoresEncontrados));
-            System.out.println("arreglo nuevoArreglo"+Arrays.toString(nuevoArreglo));
-            for (int j = 0; j < nuevoArreglo.length; j++) {
-
-            }
-
-            String[] ordenado = new String[nuevoArreglo.length];
-            int j = 0;
-            int n = 1;
-            for (int i = 0; i < nuevoArreglo.length; i++) {
-
-                String c = (nuevoArreglo[i]);
-                System.out.println("VALOR A GUARDAR " + c);
-                if ((String.valueOf(c).equals("+")) || (String.valueOf(c).equals("-"))  || (String.valueOf(c).equals("*"))|| (String.valueOf(c).equals("/"))  ) {
-                    ordenado[n] = nuevoArreglo[i];
-                    n = n + 2;
-                } else {
-                   
-
-                    ordenado[j] = nuevoArreglo[i];
-                    j = j + 2;
-                }
-
-                // System.out.println("----------------"+nuevoArreglo[j]+"----------------"+ordenado[i]+"----------------");
-            }
-
-            System.out.println(Arrays.toString(ordenado));
-
-            System.out.println("arreglo ORDENADO de operadores y tipos de variables");
-
-            for (int h=0; h < nuevoArreglo.length ; h++){
-            System.out.println ("posición "+h+" valor "+ordenado[h]);
-
-            }
-
+        } else {
+            System.out.println("" + validarLongitudTexto(texto));
         }
 
     }
@@ -268,32 +297,19 @@ public class AnalizadorLexico {
         return nuevoArreglo;
     }
 
-    // System.out.println("" + resultado + "");
-    // public static void organizarArreglo(String[] variables, char[] operadores,
-    // String[] nuevoArreglo) {
-    // int indiceVariable = 0;
-    // int indiceOperador = 1;
-    // int indiceNuevoArreglo = 0;
+    public static String validarLongitudTexto(String texto) {
+        int longitudMaxima = 20;
+        int longitudActual = texto.length();
+        int caracteresRestantes = longitudMaxima - longitudActual;
 
-    // // Recorrer los arreglos intercalando variables y operadores
-    // while (indiceVariable < variables.length && indiceOperador <
-    // operadores.length) {
-    // nuevoArreglo[indiceNuevoArreglo++] = variables[indiceVariable++];
-    // nuevoArreglo[indiceNuevoArreglo++] =
-    // String.valueOf(operadores[indiceOperador++]);
-    // }
-
-    // // Agregar las variables restantes al nuevo arreglo
-    // while (indiceVariable < variables.length) {
-    // nuevoArreglo[indiceNuevoArreglo++] = variables[indiceVariable++];
-    // }
-
-    // // Agregar los operadores restantes al nuevo arreglo
-    // while (indiceOperador < operadores.length) {
-    // nuevoArreglo[indiceNuevoArreglo++] =
-    // String.valueOf(operadores[indiceOperador++]);
-    // }
-    // }
+        if (longitudActual > longitudMaxima) {
+            return "Texto demasiado largo (" + longitudActual + " caracteres). Se permiten máximo " + longitudMaxima
+                    + " caracteres.";
+        } else {
+            return "Texto válido. Te quedan " + caracteresRestantes + " caracteres de los " + longitudMaxima
+                    + " disponibles.";
+        }
+    }
 
     public static boolean validarvariable(char[] caracteres) {
 
